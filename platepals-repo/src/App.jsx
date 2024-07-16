c7a742e5b595eeb3574858b732617d5a7118806b
n11283173 <104541077+n11283173@users.noreply.github.com>
2024-07-16 21:12:29 +1000

    created card component styling without functionality

--- platepals-repo/src/App.jsx
+++ platepals-repo/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
     return (
         <>
-            <h1>App</h1>
+            <h1>Home Page</h1>
+            <div>
+                <Card />
+            </div>
         </>
     );
 }
