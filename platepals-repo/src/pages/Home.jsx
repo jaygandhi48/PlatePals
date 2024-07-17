16f87f8f2ebe7803b0e1e37702b542a60c1906f2
n11283173 <104541077+n11283173@users.noreply.github.com>
2024-07-17 16:31:21 +1000

    create 'add' button on home screen with no funcitonality

--- platepals-repo/src/pages/Home.jsx
+++ platepals-repo/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Nav from "../components/Nav";
 import Card from "../components/Card";
+import { RiAddLargeLine } from "react-icons/ri";
+import { IconContext } from "react-icons";
 
 function Home() {
     const data = [
@@ -30,10 +32,24 @@ function Home() {
     return (
         <div>
             <Nav />
-            <div className="m-10 flex gap-10">
-                {recipeData.map((obj, index) => (
-                    <Card key={index} data={obj} />
-                ))}
+            <div className="m-10">
+                <div className=" flex gap-10 flex-wrap">
+                    {recipeData.map((obj, index) => (
+                        <Card key={index} data={obj} />
+                    ))}
+                </div>
+
+                <button className="py-3 px-3 bg-green-400 rounded-full fixed bottom-24 shadow-lg right-10 hover:bg-green-500 transition duration-300">
+                    <IconContext.Provider
+                        value={{
+                            size: "1.2em",
+                            color: "white",
+                            className: "hover:cursor-pointer ",
+                        }}
+                    >
+                        <RiAddLargeLine />
+                    </IconContext.Provider>
+                </button>
             </div>
         </div>
     );
