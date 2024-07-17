6cd30837c1fd7e1b8a842b014115821746d6fe80
jaygandhi48 <jaygandhi249@gmail.com>
2024-07-17 15:06:08 +1000

    Added footer to App

--- platepals-repo/src/App.jsx
+++ platepals-repo/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <>
             <div>
                 <Home />
+                <Footer/>
             </div>
         </>
     );
