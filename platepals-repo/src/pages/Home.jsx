72cd11d62e524a440373c15964e89d1367d30f4f
Deep0565 <104241321+Deep0565@users.noreply.github.com>
2024-07-18 02:44:06 +0000

    Added title

--- platepals-repo/src/pages/Home.jsx
+++ platepals-repo/src/pages/Home.jsx
@@ -25,6 +25,37 @@ function Home() {
             recipeName: "Fries",
             recipeDescription: "Fries with a twist",
         },
+        {
+            image: "https://images.unsplash.com/photo-1666304752980-678d5c35c911?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            profile: "Ques Freyer",
+            recipeName: "Fries",
+            recipeDescription: "Fries with a twist",
+        },
+        {
+            image: "https://images.unsplash.com/photo-1523049673857-eb18f1d7b578?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            profile: "Melvin Robson",
+            recipeName: "Avocado Salsa",
+            recipeDescription:
+                " In a few steps, you'll be able to cook a delicious AvocadoSalsa. It's easy and simple, enjoy!",
+        },
+        {
+            image: "https://images.unsplash.com/photo-1498579485796-98be3abc076e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            profile: "Betty White",
+            recipeName: "Burger",
+            recipeDescription: "Juicy Burger",
+        },
+        {
+            image: "https://images.unsplash.com/photo-1666304752980-678d5c35c911?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            profile: "Ques Freyer",
+            recipeName: "Fries",
+            recipeDescription: "Fries with a twist",
+        },
+        {
+            image: "https://images.unsplash.com/photo-1666304752980-678d5c35c911?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            profile: "Ques Freyer",
+            recipeName: "Fries",
+            recipeDescription: "Fries with a twist",
+        },
     ];
 
     const [recipeData, setRecipeData] = useState(data);
@@ -33,7 +64,8 @@ function Home() {
         <div>
             <Nav />
             <div className="m-10">
-                <div className=" flex gap-10 flex-wrap">
+                <h1 className="font-semibold text-4xl">Recipes For You</h1>
+                <div className="flex gap-10 flex-wrap">
                     {recipeData.map((obj, index) => (
                         <Card key={index} data={obj} />
                     ))}
