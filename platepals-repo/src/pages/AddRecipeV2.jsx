440f616a41bd100653a8df8b3fa11350bf37668f
n11283173 <104541077+n11283173@users.noreply.github.com>
2024-07-30 19:11:41 +1000

    changed layout to page from card

--- platepals-repo/src/pages/AddRecipeV2.jsx
+++ platepals-repo/src/pages/AddRecipeV2.jsx
@@ -93,14 +93,14 @@ function AddRecipe() {
     };
 
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center">
-            <div className="m-4 w-full max-w-lg flex-col bg-white p-8 rounded-lg shadow-lg">
+        <div className="min-h-screen flex flex-col">
+            <div className="m-4 w-full flex-col bg-white p-8">
                 <h1 className="text-3xl font-semibold mb-4">Add Recipe</h1>
                 <div className="rounded-md overflow-hidden relative mb-4">
                     <img
                         src="https://images.unsplash.com/photo-1523049673857-eb18f1d7b578?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                         alt=""
-                        className="w-full h-60 object-cover rounded-xl"
+                        className="w-full h-60 object-cover"
                     />
                     <IconContext.Provider
                         value={{
