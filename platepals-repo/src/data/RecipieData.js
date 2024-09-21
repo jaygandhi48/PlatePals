70f7d51f1c520d4dba72f1dd8651db7914c87372
jaygandhi48 <jaygandhi249@gmail.com>
2024-09-21 14:05:31 +1000

    data file seperated

---
+++ platepals-repo/src/data/RecipieData.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1523049673857-eb18f1d7b578?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    profile: "Melvin Robson",
+    recipeName: "Avocado Salsa",
+    recipeDescription:
+      " In a few steps, you'll be able to cook a delicious AvocadoSalsa. It's easy and simple, enjoy!",
+    tags: ["Vegan", "Vegetarian"],
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1498579485796-98be3abc076e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    profile: "Betty White",
+    recipeName: "Burger",
+    recipeDescription: "Juicy Burger",
+    tags: ["High Protein", "Gluten Free"],
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1666304752980-678d5c35c911?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    profile: "Ques Freyer",
+    recipeName: "Fries",
+    recipeDescription: "Fries with a twist",
+    tags: [],
+  },
+];
+export default data;
--- platepals-repo/src/pages/Home.jsx
+++ platepals-repo/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import { useState, useTransition } from "react";
 import Nav from "../components/Nav";
 import Card from "../components/Card";
+import data from "../data/RecipieData.js";
 
 function Home() {
   const [value, setValue] = useState("");
   const [startTransition, ispending] = useTransition();
-  console.log(value);
+  //console.log(value);
 
   const setKey = (e) => {
     setValue(e.target.value);
@@ -19,34 +20,6 @@ function Home() {
 
     setRecipeData(filteredRecipies);
   };
-
-  const data = [
-    {
-      image:
-        "https://images.unsplash.com/photo-1523049673857-eb18f1d7b578?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      profile: "Melvin Robson",
-      recipeName: "Avocado Salsa",
-      recipeDescription:
-        " In a few steps, you'll be able to cook a delicious AvocadoSalsa. It's easy and simple, enjoy!",
-      tags: ["Vegan", "Vegetarian"],
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1498579485796-98be3abc076e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      profile: "Betty White",
-      recipeName: "Burger",
-      recipeDescription: "Juicy Burger",
-      tags: ["High Protein", "Gluten Free"],
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1666304752980-678d5c35c911?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      profile: "Ques Freyer",
-      recipeName: "Fries",
-      recipeDescription: "Fries with a twist",
-      tags: [],
-    },
-  ];
 
   const [recipeData, setRecipeData] = useState(data);
 
